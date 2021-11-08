a9bb642a4cca55be4349cf893bb548e03580b1be
juancamiloqhz <juancamiloqhz@gmail.com>
2021-11-08 01:56:52 +0000

    setting up i18

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,13 @@
 const YEAR = new Date().getFullYear()
 
 export default {
+  i18n: [
+    { locale: 'en', text: 'English' },
+    { locale: 'es', text: 'Espanol' },
+    // { locale: 'zh', text: '中文' },
+    // { locale: 'de', text: 'Deutsch' },
+    // { locale: 'ar', text: 'العربية', direction: 'rtl' },
+  ],
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Juan Camilo QHz.
