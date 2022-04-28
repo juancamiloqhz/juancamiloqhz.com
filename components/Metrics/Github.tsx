0cec00fa78e89f7c3999fbe1412fff12906a603b
JuanCamiloQHz <juancamiloqhz@gmail.com>
2022-04-28 10:00:24 -0500

    adding github and gumroad api endpoints. dashboard page to handle github, gumroad and other apis

---
+++ components/Metrics/Github.tsx
@@ -0,0 +1,29 @@
+import useSWR from 'swr';
+
+import fetcher from 'lib/fetcher';
+import MetricCard from 'components/Metrics/Card';
+import { GitHub } from 'lib/types';
+
+export default function GitHubCard() {
+  const { data } = useSWR<GitHub>('/api/github', fetcher);
+
+  const stars = new Number(data?.stars);
+  const followers = new Number(data?.followers);
+  const link = 'https://github.com/juancamiloqhz';
+  // console.log(data);
+
+  return (
+    // <MetricCard
+    //   header="GitHub Stars"
+    //   link={link}
+    //   metric={stars}
+    //   isCurrency={false}
+    // />
+    <MetricCard
+      header="GitHub Followers"
+      link={link}
+      metric={followers}
+      isCurrency={false}
+    />
+  );
+}
