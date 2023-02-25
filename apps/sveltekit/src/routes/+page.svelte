5beabf5958f09d02fba4dbb01bda46d4e3b9ff84
juancamiloqhz <juancamiloqhz@gmail.com>
2023-02-25 08:25:27 -0500

    removing sveltekit app and turbo monorepo. Updating all dependencies to latest

--- apps/sveltekit/src/routes/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1 class="text-4xl font-bold">Welcome to JuanCamiloQHz</h1>
-<p>Visit <a href="https://juancamiloqhz.com" class="link">juancamiloqhz.com</a> to see production site</p>
--- apps/sveltekit/src/routes/guestbook/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Guestbook</h1>
