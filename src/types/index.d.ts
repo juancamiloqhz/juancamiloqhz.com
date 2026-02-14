7e6a53b30936a7f1ad9b24316c993b337fab5a08
Juan Camilo QHz <juancamiloqhz@gmail.com>
2026-02-14 00:31:57 -0500

    remove unused NavItem and MainNavItem types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,11 +1,3 @@
-export type NavItem = {
-  title: string
-  href: string
-  disabled?: boolean
-}
-
-export type MainNavItem = NavItem
-
 export type SiteConfig = {
   name: string
   description: string
